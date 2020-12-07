518114e5a237fb193840143abce686c2ec659856
ivillaverd <ignaciovillaverdeok@gmail.com>
2020-12-06 23:42:38 -0300

    Add visual elements (bootstrap)

--- script.js
+++ script.js
@@ -40,7 +40,7 @@ function loadLabeledImages() {
   return Promise.all(
     labels.map(async label => {
       const descriptions = []
-      for (let i = 1; i <= 2; i++) {
+      for (let i = 1; i <= 3; i++) {
         const img = await faceapi.fetchImage(`https://raw.githubusercontent.com/ivillaverd/Face-Recognition-JavaScript/master/labeled_images/${label}/${i}.jpg`)
         const detections = await faceapi.detectSingleFace(img).withFaceLandmarks().withFaceDescriptor()
         descriptions.push(detections.descriptor)
