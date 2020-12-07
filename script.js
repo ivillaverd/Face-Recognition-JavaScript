5b893641696e7da0b77dd084157dc3ed78149129
ivillaverd <ignaciovillaverdeok@gmail.com>
2020-12-06 22:23:50 -0300

    Change folder name to maradona

--- script.js
+++ script.js
@@ -36,7 +36,7 @@ async function start() {
 }
 
 function loadLabeledImages() {
-  const labels = ['Black Widows', 'Captain America', 'Captain Marvel', 'Hawkeye', 'Jim Rhodes', 'Thor', 'Tony Stark']
+  const labels = ['Maradona', 'Captain America', 'Captain Marvel', 'Hawkeye', 'Jim Rhodes', 'Thor', 'Tony Stark']
   return Promise.all(
     labels.map(async label => {
       const descriptions = []
